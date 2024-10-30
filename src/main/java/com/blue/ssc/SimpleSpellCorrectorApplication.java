85f555cf88c4e926a9aa39306d13d2531fedce80
black6765 <black6765@gmail.com>
2024-10-30 22:44:06 +0900

    setup: set spring boot project

---
+++ src/main/java/com/blue/ssc/SimpleSpellCorrectorApplication.java
@@ -0,0 +1,13 @@
+package com.blue.ssc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSpellCorrectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleSpellCorrectorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blue/ssc/SimpleSpellCorrectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blue.ssc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSpellCorrectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
